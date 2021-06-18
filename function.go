4a8c9269fb3b51c92020cfbd5da6c076fcfc91d5
Lee Su Jin <jean0518@naver.com>
2021-06-18 17:19:09 +0900

    배열

--- function.go
+++ function.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	say("This", "is", "a", "book")
-	say("Hi")
-}
-
-func say(msg ...string) {
-	for index, _ := range msg {
-		fmt.Println(index)
+	a := []int{1, 2, 3}
+	var b []int
+	//for 사용하여 a찍기
+	for i := 0; i < 3; i++ {
+		fmt.Print(a[i])
+	}
+	//range 사용하여 b배열찍기
+	for _, name := range b {
+		fmt.Print(name)
 	}
 }
