4fe54d005022101cefb276818c9fe9884b70af57
Lee Su Jin <jean0518@naver.com>
2021-06-18 16:48:37 +0900

    say HELLO

--- function.go
+++ function.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var names [3]string = [3]string{"김춘삼", "이순신", "홍길동"}
-
-	for index, name := range names {
-		fmt.Println(index, name)
-	}
-	몫, 성공여부 := Divide(3, 1)
-	fmt.Println(몫, 성공여부)
+	say("This", "is", "a", "book")
+	say("Hi")
 }
-func Divide(a, b int) (int, bool) {
-	// 젯수가 0이면 나눌 수 없음
-	if b == 0 {
-		return 0, false
+
+func say(msg ...string) {
+	for index, _ := range msg {
+		fmt.Println(index)
 	}
-	// 나누기 가능
-	return a / b, true
 }
