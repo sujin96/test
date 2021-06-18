38fbfb8fdae889e3e22c732810a585687841c507
Lee Su Jin <jean0518@naver.com>
2021-06-18 15:44:53 +0900

    실험1

--- function.go
+++ function.go
@@ -5,12 +5,15 @@ import "fmt"
 func main() {
 	a := 1
 	b := 2
+	fmt.Println("호출전")
+	fmt.Printf("a = %p, b = %p\n", &a, &b)
+	swap(&a, &b)
+	fmt.Println("호출후")
 	fmt.Printf("a = %d, b = %d\n", a, b)
-	swap(a, b)
 }
-func swap(i int, j int) {
-	temp := i
-	i = j
-	j = temp
-	fmt.Printf("i = %d, j = %d\n", i, j)
+func swap(i *int, j *int) {
+	temp := *i
+	*i = *j
+	*j = temp
+	fmt.Printf("i = %d, j = %d\n", *i, *j)
 }
